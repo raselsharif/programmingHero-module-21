1ad824d18d1b17f7efa675319ff5c60707cb0c7c
raselsharif <raselsharif9@gmail.com>
2023-08-05 18:54:43 +0600

    added who is tallest

---
+++ whoIsTallest.js
@@ -0,0 +1,15 @@
+
+function tallest(numbers) {
+    let largest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        let element = numbers[i];
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
+}
+
+const numbers = [167, 190, 120, 165, 137,10,5,7,307,200,250,297]
+const tallestIs = tallest(numbers);
+console.log(tallestIs);
